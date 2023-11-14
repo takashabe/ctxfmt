2433b329aa42616f42d9c2310b671b25af3bb58b
takashabe <i@takashabe.dev>
2023-11-14 11:19:21 +0900

    feat: Add interface support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/takashabe/recv-context
 
 go 1.21.4
+
+require golang.org/x/tools v0.15.0
--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"go/ast"
-	"go/parser"
 	"go/token"
 	"os"
-	"unicode"
 )
 
 var ignoreFiles = []string{
@@ -16,8 +13,9 @@ var ignoreFiles = []string{
 type inspectType string
 
 const (
-	inspectTypeInterface inspectType = "interface"
-	inspectTypeStruct    inspectType = "method"
+	inspectTypeInterface  inspectType = "interface"
+	inspectTypeInterface2 inspectType = "rewrite_interface"
+	inspectTypeMethod     inspectType = "method"
 )
 
 func main() {
@@ -25,41 +23,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	fs := token.NewFileSet()
-	for _, arg := range os.Args[2:] {
-		file, err := parser.ParseFile(fs, arg, nil, 0)
-		if err != nil {
-			continue
+	switch typ := os.Args[1]; typ {
+	case string(inspectTypeInterface):
+		fs := token.NewFileSet()
+		for _, arg := range os.Args[2:] {
+			if err := inspectInterface(fs, arg); err != nil {
+				panic(err)
+			}
 		}
-
-		if isIgnoreFile(fs.Position(file.Pos()).Filename) {
-			continue
+	case string(inspectTypeInterface2):
+		fs := token.NewFileSet()
+		for _, arg := range os.Args[2:] {
+			if err := rewirteInterface(fs, arg); err != nil {
+				panic(err)
+			}
 		}
-
-		ast.Inspect(file, func(n ast.Node) bool {
-			fn, ok := n.(*ast.FuncDecl)
-			if !ok || fn.Name == nil {
-				return true
+	case string(inspectTypeMethod):
+		fs := token.NewFileSet()
+		for _, arg := range os.Args[2:] {
+			if err := inspectMethod(fs, arg); err != nil {
+				panic(err)
 			}
-
-			if fn.Recv == nil {
-				return false
-			}
-
-			if fn.Type.Params == nil || len(fn.Type.Params.List) == 0 {
-				return false
-			}
-
-			if !unicode.IsUpper(rune(fn.Name.Name[0])) {
-				return false
-			}
-
-			if !hasContextParam(fn.Type.Params.List) {
-				pos := fs.Position(fn.Pos())
-				report(pos.Filename, fn.Name.Name, fn.Recv, pos.Line)
-			}
-
-			return false
-		})
+		}
+	default:
+		panic("invalid type")
 	}
 }
