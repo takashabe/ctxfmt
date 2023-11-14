d57e4489ae509fd364e3f4a4fe50fccc522b71a5
takashabe <i@takashabe.dev>
2023-11-14 21:48:02 +0900

    Add user-friendly cli interface

--- analyze.go
+++ analyze.go
@@ -14,32 +14,21 @@ import (
 	"golang.org/x/tools/go/ast/astutil"
 )
 
-func inspectInterface(fs *token.FileSet, filename string) error {
-	file, err := parser.ParseFile(fs, filename, nil, 0)
+func fmtInterface(fs *token.FileSet, filename string, dryrun bool) error {
+	info, err := os.Stat(filename)
 	if err != nil {
+		return err
+	}
+	if info.IsDir() {
+		return nil
+	}
+	if !strings.HasSuffix(filename, ".go") {
+		return nil
+	}
+	if isIgnoreFile(info.Name()) {
 		return nil
 	}
 
-	ast.Inspect(file, func(n ast.Node) bool {
-		if typeSpec, ok := n.(*ast.TypeSpec); ok {
-			if interfaceType, ok := typeSpec.Type.(*ast.InterfaceType); ok {
-				for _, m := range interfaceType.Methods.List {
-					for _, name := range m.Names {
-						if !hasContextParam(m.Type.(*ast.FuncType).Params.List) {
-							pos := fs.Position(m.Pos())
-							reportInterface(pos.Filename, name.Name, typeSpec, pos.Line)
-						}
-					}
-				}
-			}
-		}
-		return true
-	})
-
-	return nil
-}
-
-func rewirteInterface(fs *token.FileSet, filename string) error {
 	file, err := parser.ParseFile(fs, filename, nil, parser.ParseComments)
 	if err != nil {
 		return err
@@ -54,15 +43,20 @@ func rewirteInterface(fs *token.FileSet, filename string) error {
 				for _, m := range interfaceType.Methods.List {
 					if method, ok := m.Type.(*ast.FuncType); ok {
 						if !hasContextParam(method.Params.List) {
-							contextParam := &ast.Field{
-								Names: []*ast.Ident{ast.NewIdent("ctx")},
-								Type: &ast.SelectorExpr{
-									X:   ast.NewIdent("context"),
-									Sel: ast.NewIdent("Context"),
-								},
+							if dryrun {
+								pos := fs.Position(m.Pos())
+								reportInterface(pos.Filename, m.Names[0].Name, typeSpec, pos.Line)
+							} else {
+								contextParam := &ast.Field{
+									Names: []*ast.Ident{ast.NewIdent("ctx")},
+									Type: &ast.SelectorExpr{
+										X:   ast.NewIdent("context"),
+										Sel: ast.NewIdent("Context"),
+									},
+								}
+								method.Params.List = append([]*ast.Field{contextParam}, method.Params.List...)
+								isApply = true
 							}
-							method.Params.List = append([]*ast.Field{contextParam}, method.Params.List...)
-							isApply = true
 						}
 					}
 				}
@@ -87,44 +81,23 @@ func rewirteInterface(fs *token.FileSet, filename string) error {
 	return nil
 }
 
-func inspectMethod(fs *token.FileSet, filename string) error {
-	file, err := parser.ParseFile(fs, filename, nil, 0)
+func fmtMethod(fs *token.FileSet, filename string, dryrun bool) error {
+	info, err := os.Stat(filename)
 	if err != nil {
+		return err
+	}
+	if info.IsDir() {
+		return nil
+	}
+	if !strings.HasSuffix(filename, ".go") {
+		return nil
+	}
+	if isIgnoreFile(info.Name()) {
 		return nil
 	}
 
-	if isIgnoreFile(fs.Position(file.Pos()).Filename) {
-		return nil
-	}
-
-	ast.Inspect(file, func(n ast.Node) bool {
-		fn, ok := n.(*ast.FuncDecl)
-		if ok && fn.Name != nil {
-			if !isIgnoreFuncName(fn.Name.Name) {
-				if fn.Recv != nil {
-					if fn.Type.Params != nil && len(fn.Type.Params.List) > 0 {
-						if unicode.IsUpper(rune(fn.Name.Name[0])) {
-							if !hasContextParam(fn.Type.Params.List) {
-								pos := fs.Position(fn.Pos())
-								reportMethod(pos.Filename, fn.Name.Name, fn.Recv, pos.Line)
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return true
-	})
-	return nil
-}
-
-func inspectMethod2(fs *token.FileSet, filename string) error {
 	file, err := parser.ParseFile(fs, filename, nil, parser.ParseComments)
 	if err != nil {
-		return nil
-	}
-	if isIgnoreFile(fs.Position(file.Pos()).Filename) {
 		return nil
 	}
 
@@ -139,15 +112,20 @@ func inspectMethod2(fs *token.FileSet, filename string) error {
 					if fn.Type.Params != nil && len(fn.Type.Params.List) > 0 {
 						if unicode.IsUpper(rune(fn.Name.Name[0])) {
 							if !hasContextParam(fn.Type.Params.List) {
-								contextParam := &ast.Field{
-									Names: []*ast.Ident{ast.NewIdent("ctx")},
-									Type: &ast.SelectorExpr{
-										X:   ast.NewIdent("context"),
-										Sel: ast.NewIdent("Context"),
-									},
+								if dryrun {
+									pos := fs.Position(fn.Pos())
+									reportMethod(pos.Filename, fn.Name.Name, fn.Recv, pos.Line)
+								} else {
+									contextParam := &ast.Field{
+										Names: []*ast.Ident{ast.NewIdent("ctx")},
+										Type: &ast.SelectorExpr{
+											X:   ast.NewIdent("context"),
+											Sel: ast.NewIdent("Context"),
+										},
+									}
+									fn.Type.Params.List = append([]*ast.Field{contextParam}, fn.Type.Params.List...)
+									isApply = true
 								}
-								fn.Type.Params.List = append([]*ast.Field{contextParam}, fn.Type.Params.List...)
-								isApply = true
 							}
 						}
 					}
--- go.mod
+++ go.mod
@@ -1,15 +1,19 @@
-module github.com/takashabe/recv-context
+module github.com/takashabe/ctxfmt
 
 go 1.21.4
 
 require (
 	github.com/K0kubun/pp v3.0.1+incompatible
+	github.com/urfave/cli/v2 v2.25.7
 	golang.org/x/tools v0.15.0
 )
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
