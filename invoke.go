87d2427677b33176d4e7de030eaa5615ecf831e8
takashabe <i@takashabe.dev>
2023-12-14 18:03:36 +0900

    wip

--- invoke.go
+++ invoke.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"golang.org/x/tools/go/packages"
@@ -29,19 +30,22 @@ func invoke(fs *token.FileSet, filename string, packageName string) error {
 		return nil
 	}
 
-	cfg := &packages.Config{
-		Mode:  packages.NeedSyntax | packages.NeedName | packages.NeedFiles | packages.NeedTypes | packages.NeedTypesInfo,
-		Tests: false,
-	}
-	pkgs2, err := packages.Load(cfg, packageName)
+	dir, err := filepath.Abs(filename)
 	if err != nil {
 		return err
 	}
-
-	// パッケージの解析
-	pkg := pkgs2[0]
-	if len(pkg.Errors) > 0 {
-		return fmt.Errorf("packages.Load: %v", pkg.Errors)
+	cfg := &packages.Config{
+		Mode:  packages.NeedSyntax | packages.NeedName | packages.NeedFiles | packages.NeedTypes | packages.NeedTypesInfo,
+		Tests: false,
+		Dir:   dir,
+	}
+	pkg2, err := packages.Load(cfg, packageName)
+	if err != nil {
+		return err
+	}
+	pkg := pkg2[0].Errors
+	if len(pkg) > 0 {
+		return fmt.Errorf("packages.Load: %v", pkg)
 	}
 
 	return nil
@@ -68,56 +72,3 @@ func extractModuleName(goModPath string) (string, error) {
 	}
 	return "", fmt.Errorf("module directive not found in %s", goModPath)
 }
-
-// package main
-//
-// import (
-//   "go/ast"
-//   "go/parser"
-//   "go/token"
-//   "go/types"
-//   "log"
-//   "os"
-// )
-//
-// func main() {
-//   filename := "yourfile.go" // 解析するファイル
-//
-//   // ファイルセットの作成
-//   fset := token.NewFileSet()
-//
-//   // ソースファイルの解析
-//   file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-//
-//   // 型チェッカーの設定
-//   conf := types.Config{Importer: types.DefaultImporter}
-//   info := &types.Info{
-//     Types: make(map[ast.Expr]types.TypeAndValue),
-//   }
-//
-//   // ファイルに含まれるパッケージの型情報を取得
-//   pkg, err := conf.Check("main", fset, []*ast.File{file}, info)
-//   if err != nil {
-//     log.Fatal(err)
-//   }
-//
-//   // ASTをトラバースし、関数呼び出しを探す
-//   ast.Inspect(file, func(n ast.Node) bool {
-//     if callExpr, ok := n.(*ast.CallExpr); ok {
-//       fnType, ok := info.Types[callExpr.Fun].Type.(*types.Signature)
-//       if !ok {
-//         return true
-//       }
-//
-//       // 関数の引数が足りているかチェック
-//       if len(callExpr.Args) < fnType.Params().Len() {
-//         // context.Context が不足しているか確認
-//         // ...
-//       }
-//     }
-//     return true
-//   })
-// }
